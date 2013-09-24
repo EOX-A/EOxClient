c2d5f952db068c3f8fcbeb582d26d01e0084b7ee
Daniel Santillan <daniel.santillan@eox.at>
2013-09-24 15:27:46 +0200

    Added new version of TimeSlider

--- app/scripts/views/TimeSliderView.js
+++ app/scripts/views/TimeSliderView.js
@@ -10,7 +10,7 @@
   ],
   function( Backbone, Communicator, timeslider) {
     var TimeSliderView = Backbone.Marionette.ItemView.extend({
-      className: 'timeslider',
+      id: 'timeslider',
       events: {
         'selectionChanged': 'onChangeTime'
       },
@@ -22,8 +22,10 @@
         var selectionend = new Date("2013-06-08T00:00:00Z");
         var slider = new TimeSlider(this.el, {
 
-          start: new Date("2012-07-01T00:00:00Z"),
-          end: new Date("2013-07-01T00:00:00Z"),
+          domain: {
+            start: new Date("2012-07-01T00:00:00Z"),
+            end: new Date("2013-07-01T00:00:00Z")
+          },
           brush: {
             start: selectionstart,
             end: selectionend
