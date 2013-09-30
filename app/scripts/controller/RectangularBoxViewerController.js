b5d48ccd2056cdeda122377d467b0690916f9044
Martin Hecher <martin.hecher@fraunhofer.at>
2013-09-30 14:22:27 +0200

    Added 'history' support for RectangularBoxViewer and Map

---
+++ app/scripts/controller/RectangularBoxViewerController.js
@@ -0,0 +1,69 @@
+define([
+	'backbone',
+	'communicator',
+	'app',
+	'views/RectangularBoxView'
+], function(Backbone, Communicator, App, RectangularBoxView) {
+
+	'use strict';
+
+	App.module('RectangularBoxViewer', function(Module) {
+
+		this.startsWithParent = true;
+
+		Module.Controller = Backbone.Marionette.Controller.extend({
+
+			initialize: function(options) {
+				this.rbvView = undefined;
+			},
+
+			show: function() {
+				if (typeof(this.rbvView) == 'undefined') {
+					this.rbvView = new RectangularBoxView({
+						el: $('#x3dom')
+					});
+				}
+
+				App.map.show(this.rbvView);
+			}
+		});
+
+		this.on('start', function(options) {
+			var controller = new Module.Controller();
+
+			registerWithCommunicator(controller);
+			setupRouter(controller);
+
+			console.log('[VirtualGlobeViewer] Finished module initialization');
+
+		});
+
+		var registerWithCommunicator = function(rbv_controller) {
+			Communicator.registerEventHandler("viewer:show:rectangularboxviewer", function() {
+				App.router.navigate("rbv", {
+					trigger: true
+				});
+			});
+		};
+
+		var setupRouter = function(rbv_controller) {
+			var Router = Backbone.Marionette.AppRouter.extend({
+				appRoutes: {
+					"rbv": "show"
+				}
+			});
+
+			var RouteController = function() {};
+
+			_.extend(RouteController.prototype, {
+				show: function() {
+					rbv_controller.show();
+				}
+			});
+
+			new Router({
+				controller: new RouteController(rbv_controller)
+			});
+		};
+	});
+});
