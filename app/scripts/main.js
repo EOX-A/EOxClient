85c7126da4f22ddee94ae1e0bb17c4ad9743c64b
Daniel Santillan <daniel.santillan@eox.at>
2013-10-23 14:32:45 +0200

    Separated data config file, to be able to load from another server. Updated libcoverage library, please do an bower update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -31,44 +31,52 @@
 		"libcoverage"
 	],
 	function ( Backbone, App ) {
+
+		
+
 		$.get("scripts/config.json", function(values) {
-			
-			// Configure Debug options
-			setuplogging(values.debug);
 
-			var viewModules = [];
-			var models = [];
-			var templates = [];
-			var options = {};
-			var config = {};
+			var origvalues = values;
 
-			_.each(values.views, function(view) {
-				viewModules.push(view);
-			}, this);
+			$.get(values.mapConfig.dataconfigurl, function(values) {
+				
+				_.extend(origvalues.mapConfig, values);
 
-			_.each(values.models, function(model) {
-				models.push(model);
-			}, this);
+					// Configure Debug options
+				setuplogging(values.debug);
 
-			_.each(values.templates, function(tmpl) {
-				templates.push(tmpl.template);
-			}, this);
+				var viewModules = [];
+				var models = [];
+				var templates = [];
+				var options = {};
+				var config = {};
 
-			root.require([].concat(
-				values.mapConfig.visualizationLibs, 	//Visualizations such as Openlayers or GlobWeb
-				values.mapConfig.module, 				//Which module should be used for map visualization
-				values.mapConfig.model,					//Which model to use for saving map data
-				viewModules,							//All "activated" views are loaded
-				models,
-				templates
-			), function(){
-				App.configure(values);
-				App.start();
+				_.each(origvalues.views, function(view) {
+					viewModules.push(view);
+				}, this);
+
+				_.each(origvalues.models, function(model) {
+					models.push(model);
+				}, this);
+
+				_.each(origvalues.templates, function(tmpl) {
+					templates.push(tmpl.template);
+				}, this);
+
+				root.require([].concat(
+					origvalues.mapConfig.visualizationLibs, 	//Visualizations such as Openlayers or GlobWeb
+					origvalues.mapConfig.module, 				//Which module should be used for map visualization
+					origvalues.mapConfig.model,					//Which model to use for saving map data
+					viewModules,							//All "activated" views are loaded
+					models,
+					templates
+				), function(){
+					App.configure(origvalues);
+					App.start();
+				});
+
 			});
 
-			
-
-				
 		});
 		
 	});
