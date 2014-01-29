97bc2e8be2e176e37ef9e574226fcf7c293e9ad5
Martin Hecher <martin.hecher@fraunhofer.at>
2014-01-29 10:53:04 +0100

    Grunt: Added RBV source file to watch task to ease development

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,8 @@ module.exports = function (grunt) {
                     '{.tmp,<%= yeoman.app %>}/scripts/**/*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
                     '<%= yeoman.app %>/scripts/config.json',
-                    '<%= yeoman.app %>/templates/{,*/}*.hbs'
+                    '<%= yeoman.app %>/templates/{,*/}*.hbs',
+                    '<%= yeoman.app %>/bower_components/rectangularboxviewer/js/EarthServerClient_DailyBuild.js'
                 ]
             }
         },
